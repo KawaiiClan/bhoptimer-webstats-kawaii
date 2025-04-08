7741f9b9dda3309a4654eaa3c17f5bdcac13a1d7
olivia <liv@olivia.cx>
2025-04-08 11:17:00 -0700

    ts-bar.php - add $sv variable

--- assets/php/ts-bar.php
+++ assets/php/ts-bar.php
@@ -1,4 +1,5 @@
 <form action="index.php" method="GET" style=" display:inline!important;">
+	<input type="hidden" name="sv" id="sv" value="<?=$sv?>"/>
 	<?=($rt)?'<input type="hidden" name="rt" id="rt" value="1"/>':''?>
 	<?=($rr)?'<input type="hidden" name="rr" id="rr" value="1"/>':''?>
 	<?=(isset($u))?'<input type="hidden" name="u" id="u" value="'.$u.'"/>':''?>
@@ -18,6 +19,7 @@
 </form>
 
 <form action="index.php" method="GET" style="display:inline!important;">
+	<input type="hidden" name="sv" id="sv" value="<?=$sv?>"/>
 	<?=($rt)?'<input type="hidden" name="rt" id="rt" value="1"/>':''?>
 	<?=($rr)?'<input type="hidden" name="rr" id="rr" value="1"/>':''?>
 	<?=(isset($u))?'<input type="hidden" name="u" id="u" value="'.$u.'"/>':''?>
